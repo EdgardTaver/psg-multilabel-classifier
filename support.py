d4c69367bfefc8228d8e2650d894afec1a81b268
Edgard Taver <edgard.taver@sumup.com>
2023-10-13 23:02:17 +0200

    uses formatting

--- support.py
+++ support.py
@@ -5,7 +5,7 @@ import pandas as pd
 from sklearn.feature_selection import f_classif
 
 
-class CalculateLabelsCorrelationWithFTest():
+class CalculateLabelsCorrelationWithFTest:
     def __init__(
         self,
         alpha: float = 0.5,
@@ -18,14 +18,19 @@ class CalculateLabelsCorrelationWithFTest():
     def get(self, y: Any):
         labels_count = y.shape[1]
 
-        f_tested_label_pairs = self.calculate_f_test_for_all_label_pairs(labels_count, y)
-        
+        f_tested_label_pairs = self.calculate_f_test_for_all_label_pairs(
+            labels_count, y
+        )
+
         correlated_labels_map = self.get_data_frame_of_correlated_labels(
-            labels_count, f_tested_label_pairs)
+            labels_count, f_tested_label_pairs
+        )
 
         return correlated_labels_map
 
-    def calculate_f_test_for_all_label_pairs(self, labels_count: int, y: Any) -> List[Dict[str, Any]]:
+    def calculate_f_test_for_all_label_pairs(
+        self, labels_count: int, y: Any
+    ) -> List[Dict[str, Any]]:
         results = []
         for i in range(0, labels_count):
             for j in range(0, labels_count):
@@ -40,11 +45,13 @@ class CalculateLabelsCorrelationWithFTest():
 
                 f_test_result = f_classif(base_label, against_label)[0]
 
-                results.append({
-                    "label_being_tested": i,
-                    "against_label": j,
-                    "f_test_result": float(f_test_result)
-                })
+                results.append(
+                    {
+                        "label_being_tested": i,
+                        "against_label": j,
+                        "f_test_result": float(f_test_result),
+                    }
+                )
 
         return results
 
@@ -54,12 +61,14 @@ class CalculateLabelsCorrelationWithFTest():
     def convert_matrix_to_vector(self, matrix: Any):
         return np.asarray(matrix).reshape(-1)
 
-    def get_data_frame_of_correlated_labels(self, labels_count: int, f_test_results: List[Dict[str, Any]]) -> pd.DataFrame:
+    def get_data_frame_of_correlated_labels(
+        self, labels_count: int, f_test_results: List[Dict[str, Any]]
+    ) -> pd.DataFrame:
         temp_df = pd.DataFrame(f_test_results)
 
         sorted_temp_df = temp_df.sort_values(
-            by=["label_being_tested", "f_test_result"],
-            ascending=[True, False])
+            by=["label_being_tested", "f_test_result"], ascending=[True, False]
+        )
         # ordering in descending order by the F-test result,
         # following what the main article describes
 
@@ -78,11 +87,13 @@ class CalculateLabelsCorrelationWithFTest():
                 if cum_f > max_cum_f:
                     break
 
-                selected_features.append({
-                    "for_label": i,
-                    "expand_this_label": int(row["against_label"]),
-                    "f_test_result": float(row["f_test_result"]),
-                })
+                selected_features.append(
+                    {
+                        "for_label": i,
+                        "expand_this_label": int(row["against_label"]),
+                        "f_test_result": float(row["f_test_result"]),
+                    }
+                )
 
         cols = ["for_label", "expand_this_label", "f_test_result"]
         return pd.DataFrame(selected_features, columns=cols)
