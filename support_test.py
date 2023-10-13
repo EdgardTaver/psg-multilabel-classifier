76640d862f847c8042bdac0604112310c6283012
Edgard Taver <edgard.taver@sumup.com>
2023-10-13 23:29:28 +0200

    moves where the file is declarated

--- support_test.py
+++ support_test.py
@@ -2,18 +2,19 @@ from skmultilearn.dataset import load_dataset
 from support import CalculateLabelsCorrelationWithFTest
 import pandas as pd
 from helper import expect_data_frames_to_be_equal
+import os
 
 
-def test_bla():
+def test_calculate_labels_correlation_with_f_test():
     train_data = load_dataset("scene", "train")
-    test_data = load_dataset("scene", "test")
-
     X_train, y_train, _, _ = train_data
-    X_test, y_test, _, _ = test_data
 
     ccf = CalculateLabelsCorrelationWithFTest(alpha=1)
     res = ccf.fit(X_train, y_train)
 
-    expected = pd.read_csv("bla.csv")
+    f = os.path.join(
+        "test_data", "expected_calculate_labels_correlation_with_f_test.csv"
+    )
+    expected = pd.read_csv(f)
 
     expect_data_frames_to_be_equal(res, expected)
