9f4086f05ed900d1e34da41e60c3a3bbcb5bec24
Edgard Taver <edgard.vtt@gmail.com>
2023-10-30 22:42:32 -0300

    adds optimization pipeline

--- lib/types.py
+++ lib/types.py
@@ -5,9 +5,14 @@ from skmultilearn.base.problem_transformation import ProblemTransformationBase
 
 class MultiLabelClassifier(ABC, ProblemTransformationBase):
     @abstractmethod
-    def fit(self, X: Any, y: Any):
+    def fit(self, X: Any, y: Any) -> None:
         pass
     
     @abstractmethod
-    def predict(self, X: Any):
+    def predict(self, X: Any) -> Any:
+        pass
+
+class OptimizationPipeline(ABC):
+    @abstractmethod
+    def run(self, X: Any, y: Any) -> Any:
         pass
