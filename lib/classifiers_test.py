8318c57cf501b921ed5bb331bf1c39aa0d3a9cb6
Edgard Taver <edgard.vtt@gmail.com>
2023-11-20 17:54:55 -0300

    adds tests for both types of orderings

--- lib/classifiers_test.py
+++ lib/classifiers_test.py
@@ -34,7 +34,12 @@ def test_stacking_with_f_tests():
     assert hamming_loss == 0.0862597547380156
     assert f1_score == 0.7105885090069132
 
-def test_classifier_chain_with_f_test_ordering():
+def test_classifier_chain_with_f_test_ordering_regular():
+    """
+    Uses the regular ordering of the chain, which is the descending order
+    of the F-Test correlations.
+    """
+
     dataset_name = "scene"
     
     train_dataset = load_dataset(dataset_name, "train")
@@ -48,6 +53,7 @@ def test_classifier_chain_with_f_test_ordering():
     X_test, y_test, _, _ = test_dataset
 
     model = ClassifierChainWithFTestOrdering(
+        ascending_chain=False,
         base_classifier=RandomForestClassifier(random_state=42),
     )
     model.fit(X_train, y_train)
@@ -58,6 +64,37 @@ def test_classifier_chain_with_f_test_ordering():
 
     assert hamming_loss == 0.08876811594202899
     assert f1_score == 0.6952594727561733
+
+def test_classifier_chain_with_f_test_ordering_ascending():
+    """
+    Uses the ascending ordering of the chain, which is the ascending order
+    of the F-Test correlations.
+    """
+
+    dataset_name = "scene"
+    
+    train_dataset = load_dataset(dataset_name, "train")
+    if train_dataset is None:
+        raise Exception("could not load dataset")
+    X_train, y_train, _, _ = train_dataset
+
+    test_dataset = load_dataset(dataset_name, "test")
+    if test_dataset is None:
+        raise Exception("could not load dataset")
+    X_test, y_test, _, _ = test_dataset
+
+    model = ClassifierChainWithFTestOrdering(
+        ascending_chain=True,
+        base_classifier=RandomForestClassifier(random_state=42),
+    )
+    model.fit(X_train, y_train)
+    predictions = model.predict(X_test)
+
+    hamming_loss = metrics.hamming_loss(y_test, predictions)
+    f1_score = metrics.f1_score(y_test, predictions, average="macro")
+
+    assert hamming_loss == 0.08946488294314381
+    assert f1_score == 0.6900349522611511
 
 def test_classifier_chain_with_genetic_algorithm():
     dataset_name = "emotions"
