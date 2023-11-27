faed20170d6ad8f8e844f2d1424beafc43ec09f1
Edgard Taver <edgard.vtt@gmail.com>
2023-11-27 09:43:44 -0300

    runs one more test

--- main_knn_normalized.py
+++ main_knn_normalized.py
@@ -22,7 +22,7 @@ def setup_logging() -> None:
     logging.basicConfig(level=logging.INFO, format=LOGGING_FORMAT)
 
 
-N_FOLDS = 10
+N_FOLDS = 5
 BASE_FILE_NAME = f"knn_normalized_n_folds={N_FOLDS}"
 PIPELINE_RESULTS_FILE = f"./data/metrics_{BASE_FILE_NAME}.csv"
 SUMMARIZED_RESULTS_FILE = f"./data/summarized_result_{BASE_FILE_NAME}.csv"
