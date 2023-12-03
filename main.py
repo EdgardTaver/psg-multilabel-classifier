ce218963951b979e8cbf80327fa22922a13a0bd4
Edgard Taver <edgard.vtt@gmail.com>
2023-12-03 15:00:58 -0300

    adds more versions of the partial cc to run

--- main.py
+++ main.py
@@ -112,6 +112,31 @@ def build_models_list() -> Dict[str, Any]:
             base_classifier=RandomForestClassifier(random_state=42),
             num_generations=50,
         ),
+        "partial_classifier_chain_with_lop-num_generations=10-threshold=0.001": PartialClassifierChainWithLOP(
+            base_classifier=RandomForestClassifier(random_state=42),
+            num_generations=10,
+            threshold=0.001,
+        ),
+        "partial_classifier_chain_with_lop-num_generations=10-threshold=0.01": PartialClassifierChainWithLOP(
+            base_classifier=RandomForestClassifier(random_state=42),
+            num_generations=10,
+            threshold=0.01,
+        ),
+        "partial_classifier_chain_with_lop-num_generations=10-threshold=0.025": PartialClassifierChainWithLOP(
+            base_classifier=RandomForestClassifier(random_state=42),
+            num_generations=10,
+            threshold=0.025,
+        ),
+        "partial_classifier_chain_with_lop-num_generations=10-threshold=0.05": PartialClassifierChainWithLOP(
+            base_classifier=RandomForestClassifier(random_state=42),
+            num_generations=10,
+            threshold=0.05,
+        ),
+        "partial_classifier_chain_with_lop-num_generations=10-threshold=0.1": PartialClassifierChainWithLOP(
+            base_classifier=RandomForestClassifier(random_state=42),
+            num_generations=10,
+            threshold=0.1,
+        ),
         # "classifier_chain_with_genetic_algorithm-num_generations=5": ClassifierChainWithGeneticAlgorithm(
         #     base_classifier=RandomForestClassifier(random_state=42),
         #     num_generations=5,
