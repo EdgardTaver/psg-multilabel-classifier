918d1526316be300c8b86eea63ed5dee2709f7ba
Edgard Taver <edgard.vtt@gmail.com>
2023-12-13 19:17:28 -0300

    adds descriptive comment

--- metrics/evaluation.py
+++ metrics/evaluation.py
@@ -18,7 +18,17 @@ class EvaluationPipeline:
         accuracy_scorer = metrics.make_scorer(metrics.accuracy_score)
         hamming_loss_scorer = metrics.make_scorer(
             metrics.hamming_loss, greater_is_better=False)
+
         f1_scorer = metrics.make_scorer(metrics.f1_score, average="macro", zero_division=0)
+        # using "macro" average following what is described here:
+        # """
+        # Macro-averaging is to be preferred over micro-averaging in case of imbalanced classes
+        # (which is almost always the case), because it weighs each of the classes equally and
+        # isn’t influenced by the number of examples of each class.
+        # """
+        # source:
+        # https://medium.com/synthesio-engineering/precision-accuracy-and-f1-score-for-multi-label-classification-34ac6bdfb404
+
 
         scoring_set = {
             "accuracy": accuracy_scorer,
